294d87ac49aac512b8c6a1af913a0d031ba2dcfd
Ellis Kenyo <elken.tdos@gmail.com>
2015-06-21 20:30:21 +0100

    Lots of stuff broken. Fixed soon, honest.

--- dwm-status.sh
+++ dwm-status.sh
@@ -12,9 +12,9 @@ getBattery() {
     is_charging=$(acpi -a | awk '/Adapter/ {print $3}')
 
     if [ "${is_charging}" == "on-line" ]; then
-        bat_icons=("µ" "µ" "µ")
+        bat_icons=("" "" "")
     else
-        bat_icons=("ñ" "ò" "ó")
+        bat_icons=("" "" "")
     fi
 
     if [ ${perc} -eq "100" ]; then
@@ -31,42 +31,42 @@ getBattery() {
 getCPU() {
     cpu=$(mpstat -P ALL 1 1 | awk '/Average:/ && $2 ~ /all/ {print $3}')
     if [ $(bc <<< "${cpu}<25") == 1 ]; then
-        echo -ne "${green}Ñ"
+        echo -ne "${green}"
     elif [ $(bc <<< "${cpu}<75") == 1 ]; then
-        echo -ne "${yellow}Ñ"
+        echo -ne "${yellow}"
     else
-        echo -ne "${red}Ñ"
+        echo -ne "${red}"
     fi
 }
 
- getMEM() {
-     mem="$(free -m | awk '/-\/+/ {print $3}')"
+getMEM() {
      total=$(free -m | awk '/Mem:/ {print $2}')
+     mem="$(bc <<< ${total}-$(free -m | awk '/Mem:/ {print $7}'))"
      if [ $(bc <<< "${mem}<$(echo ${total}* .25 | bc)") == 1 ]; then
-        echo -ne "${green}Î"
+        echo -ne "${green}"
      elif [ $(bc <<< "${mem}<$(echo ${total}* .5 | bc)") == 1 ]; then
-        echo -ne "${yellow}Î"
+        echo -ne "${yellow}"
      else
-        echo -ne "${red}Î"
+        echo -ne "${red}"
      fi
  }
 
 getUpdates() {
-    upd="$(cat /etc/apt/total_updates)"
+    upd="$(cat ~/.total_updates)"
     if [ "${upd}" -le 0 ]; then
         echo -ne ""
     else
-        echo -ne "${red}§${normal} ${upd} "
+        echo -ne "${red} ${normal}${upd} "
     fi
 }
 
 getSound() {
-    is_muted=$(amixer get Master | awk '/Front Left:/ {print $6}' | tr -d '[]')
+    is_muted=$(amixer get Master | awk '/%/ {gsub(/[\[\]]/,""); print $6}' | tail -1)
     cur_device=$(pactl list sinks | awk '/Active Port:/ {print substr($3,15)}' | grep -v "^$")
     if [ ${cur_device} == "headphones" ]; then
-        out_device="à"
+        out_device="  "
     else
-        out_device="í"
+        out_device="  "
     fi
 
     if [ ${is_muted} == "on" ]; then
@@ -81,9 +81,9 @@ getMusic() {
     music_str="" 
     if [ "$(mpc current)" != "" ]; then
         if [ $(mpc | awk '/^\[/ {print $1}') == "[playing]" ]; then
-            music_str+="${blue}æ"
+            music_str+="${blue}"
         else
-            music_str+="${blue}ç"
+            music_str+="${blue}"
         fi
         if [ $(expr length "$(mpc current)") -le 50 ]; then
             music_str+=" $(mpc current) ($(mpc | head -2 | tail -1 | awk '{print $3}'))"
@@ -93,14 +93,14 @@ getMusic() {
     else
         music_str+=""
     fi
-    echo -ne "${music_str}"
+    echo -ne "${music_str} "
 }
 
 getTime() {
-    tme="$(date '+ %H:%M')"
-    echo -ne "${blue}É${normal}${tme}"
+    tme="$(date '+%A %D %H:%M')"
+    echo -ne "${blue} ${normal}${tme}"
 }
 
 while true; do
-    xsetroot -name "$(getSound)$(getMusic)$(getBattery)$(getCPU) $(getMEM) $(getUpdates)$(getTime)"
+    xsetroot -name "$(getSound)$(getMusic)$(getCPU) $(getMEM) $(getUpdates)$(getTime)"
 done
--- dwmstart.sh
+++ dwmstart.sh
@@ -6,15 +6,21 @@ setxkbmap gb
 xset -dpms
 xset s off
 xsetroot -cursor_name left_ptr
+xset +fp /usr/share/fonts/local
+xset +fp /usr/share/fonts/misc
+xset fp rehash
 
 [ ! -s ~/.config/mpd/pid ] && mpd &
+pulse --start &
+nitrogen --restore &
 wicd-gtk -t &
 emacs --daemon &
 dunst &
 thunar --daemon &
 compton &
-iceweasel &
-icedove &
+firefox &
+thunderbird &
 
 ~/bin/dwm-status.sh 2> ~/.logs/status &
-~/.dwm/dwm 2> ~/.logs/dwm
+#~/.dwm/dwm 2> ~/.logs/dwm
+~/build/dwm/dwm 2> ~/.logs/dwm
